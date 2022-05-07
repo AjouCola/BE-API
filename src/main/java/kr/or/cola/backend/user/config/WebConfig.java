9fdde02bc2d70bc2d72ce86b7baed3cb5f6254c9
sbhong <tjdqls1668@ajou.ac.kr>
2022-05-07 19:53:07 +0900

    chore: remove unused code

--- src/main/java/kr/or/cola/backend/user/config/WebConfig.java
+++ src/main/java/kr/or/cola/backend/user/config/WebConfig.java
@@ -22,17 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//            .allowedMethods("*")
-            .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+            .allowedOrigins(
+                "http://localhost:8080", "http://localhost:3000",
+                "http://ec2-3-39-4-189.ap-northeast-2.compute.amazonaws.com:8080",
+                "http://52.79.77.93:3000"
+            )
             .allowedHeaders("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowCredentials(true)
-//            .exposedHeaders("authorization")
             .maxAge(3600)
-//        registry.addMapping("/**").allowedOrigins(
-////            "http://localhost:3000", "http://52.79.77.93:3000", "http://cola.or.kr:3000",
-////            "http://ec2-3-39-4-189.ap-northeast-2.compute.amazonaws.com:8080"
-//        );
         ;
 
     }
--- src/main/java/kr/or/cola/backend/user/config/auth/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/kr/or/cola/backend/user/config/auth/OAuth2AuthenticationSuccessHandler.java
@@ -1,17 +1,10 @@
 package kr.or.cola.backend.user.config.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.net.URI;
-import java.util.Optional;
 
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
