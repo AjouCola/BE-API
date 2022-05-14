712f020667db8cb9a6f8d827c3420a39fbf38295
sbhong <tjdqls1668@ajou.ac.kr>
2022-05-14 21:10:32 +0900

    fix: add login failure handler, fix login success handler

--- src/main/java/kr/or/cola/backend/config/SecurityConfig.java
+++ src/main/java/kr/or/cola/backend/config/SecurityConfig.java
@@ -4,15 +4,21 @@ import kr.or.cola.backend.oauth.OAuth2AuthenticationSuccessHandler;
 import kr.or.cola.backend.user.UserService;
 import kr.or.cola.backend.user.domain.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+    @Value("${cola.redirect-uri.sign-in}")
+    private String signInUrl;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,5 +41,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .userInfoEndpoint()
                     .userService(userService);
 
+    }
+
+    @Bean
+    SimpleUrlAuthenticationFailureHandler failureHandler() {
+        return new SimpleUrlAuthenticationFailureHandler(signInUrl);
     }
 }
