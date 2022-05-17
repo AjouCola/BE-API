a5c9d8a881e44b79d241fa3eb3527ab37e752a80
sbhong <tjdqls1668@ajou.ac.kr>
2022-05-18 03:04:13 +0900

    feat: add base url response controller

--- src/main/java/kr/or/cola/backend/common/presentation/RootController.java
+++ src/main/java/kr/or/cola/backend/common/presentation/RootController.java
@@ -1,2 +1,13 @@
-package kr.or.cola.backend.common.presentation;public class RootController {
+package kr.or.cola.backend.common.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("COLA server is running");
+    }
 }
