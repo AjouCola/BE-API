bc259f0f4164c04c1c88f327c69cca671e43b5af
kevindec18 <kevindec18@ajou.ac.kr>
2022-05-22 23:33:54 +0900

    refactor: mail dto

--- src/main/java/kr/or/cola/backend/auth/dto/AuthMailRequestDto.java
+++ src/main/java/kr/or/cola/backend/auth/dto/AuthMailRequestDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class AuthMailRequestDto {
-    private final String email;
+    private String email;
 
     public AuthMailRequestDto(String email){
         this.email = email;
--- src/main/java/kr/or/cola/backend/auth/dto/AuthResponseDto.java
+++
@@ -1,4 +0,0 @@
-package kr.or.cola.backend.auth.dto;
-
-public class AuthResponseDto {
-}
