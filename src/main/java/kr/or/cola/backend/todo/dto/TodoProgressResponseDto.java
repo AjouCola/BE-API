718826ab00d5a57def2b9e5836205b6ae777c1a8
kevindec18 <kevindec18@ajou.ac.kr>
2022-05-30 01:38:09 +0900

    refactor: Optimize import

--- src/main/java/kr/or/cola/backend/todo/dto/TodoProgressResponseDto.java
+++ src/main/java/kr/or/cola/backend/todo/dto/TodoProgressResponseDto.java
@@ -2,11 +2,12 @@ package kr.or.cola.backend.todo.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Getter
+@NoArgsConstructor
 public class TodoProgressResponseDto {
 
     @JsonFormat(pattern = "yyyy-MM-dd")
@@ -19,56 +20,4 @@ public class TodoProgressResponseDto {
         this.todoProgress = todoProgress;
     }
 
-    public void update(List<ProgressDto> todoProgress) {
-        this.todoProgress = todoProgress;
-    }
 }
-
-
-/**
- * 1. 비정규화
- * {
- *      date : "2022-05-01"
- *     progress : [
- *          {
- *              color : "#aaaaaa",
- *              name : "할 일",
- *              progress : 0
- *          },
- *          {
- *              color : "#aaaaba",
- *              name : "한 일",
- *              progress : 100
- *          }
- *     ]
- * }
- */
-
-/**
- * 1. 비정규화
- * {
- *      date : "2022-05-01"
- *      folders : [
- *          {
- *              folderId : 1,
- *              name : "할 일",
- *              color : "#aaaaaa"
- *          },
- *          {
- *              folderId : 2,
- *              name : "한 일",
- *              color : "#aaaaaa"
- *          }
- *      ]
- *     progress : [
- *          {
- *              folderId : 1,
- *              progress : 0
- *          },
- *          {
- *              folderId : 2,
- *              progress : 100
- *          }
- *     ]
- * }
- */
